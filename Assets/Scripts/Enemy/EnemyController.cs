0f3f568b42760c1104be1beecf7eba18d25ff652
jamesbaber1 <31859220+jamesbaber1@users.noreply.github.com>
2019-02-18 17:03:34 -0600

    Inititial Commit

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    //private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        //agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "Player")
+        {
+            //agent.SetDestination(new Vector3(0,0,0));
+            Debug.Log("Hit collider");
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Camera cam;
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>() as Camera;
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
