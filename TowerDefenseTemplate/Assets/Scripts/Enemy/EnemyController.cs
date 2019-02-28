26776e6732530d0751574ff3a1ef4639587fd7ae
jamesbaber1 <31859220+jamesbaber1@users.noreply.github.com>
2019-02-27 18:17:25 -0600

    Player and Enemy milita changes
    Player can now be selected and navigated.  And you can select enemies for it to attack.  Enemys are attracked to player object within a certain radius

---
+++ TowerDefenseTemplate/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
+using System.Linq;
+
+public class EnemyController : MonoBehaviour
+{
+    
+    public float attractionRadius = 7f;
+
+
+    private GameObject chiefHouse;
+    private GameObject[] Players;
+    private GameObject NearestPlayer;
+    private float playerDistance;
+    private List<float> playerDistances = new List<float>();
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        // The enemies intial target is the chief house
+        chiefHouse = GameObject.Find("chiefHouse");
+
+        NearestPlayer = chiefHouse;
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        agent.SetDestination(NearestPlayer.transform.position);
+
+        //Get list of all players by tag
+        Players = GameObject.FindGameObjectsWithTag("Player");
+
+        //The Search frequency for new enemies
+        InvokeRepeating("findNearstPlayer", 2.0f, 0.5f);
+    }
+
+
+
+
+    void Update()
+    {
+
+        if (playerDistance < attractionRadius)
+        {
+            agent.SetDestination(NearestPlayer.transform.position);
+        }
+
+        else
+        {
+            agent.SetDestination(chiefHouse.transform.position);
+        }
+    }
+
+
+    private GameObject findNearstPlayer()
+    {
+        playerDistances.Clear();
+
+        foreach (GameObject tmpNearestPlayer in Players)
+        {
+            playerDistance = Vector3.Distance(tmpNearestPlayer.transform.position, this.transform.position);
+            playerDistances.Add(playerDistance);
+
+            if(playerDistance <= playerDistances.Min())
+            {
+                NearestPlayer = tmpNearestPlayer;
+                playerDistance = playerDistances.Min();
+            }
+
+        }
+
+        return NearestPlayer;
+
+    }
+
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    Debug.Log("Hit collider of " + collision.gameObject.name);
+
+    //    //Check for a match with the specified name on any GameObject that collides with your GameObject
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        agent.SetDestination(collision.gameObject.transform.position);
+    //        Debug.Log("Hit collider of " + collision.gameObject.name);
+    //    }
+
+    //}
+}
---
+++ TowerDefenseTemplate/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject selectRing;
+
+    private Camera cam;
+    private NavMeshAgent agent;
+    private bool playerSelected = false;
+    private bool enemySelected = false;
+    private GameObject selectedEnemy;
+
+
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>() as Camera;
+        agent = gameObject.GetComponent<NavMeshAgent>();
+
+
+        InvokeRepeating("findEnemy", 2.0f, 0.5f);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.gameObject.tag == "Player")
+                {
+                    playerSelected = true;
+                    selectRing.SetActive(true);
+                }
+
+                else
+                {
+                    playerSelected = false;
+                    selectRing.SetActive(false);
+                }
+                
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.gameObject.tag == "Enemy")
+                {
+                    selectedEnemy = hit.collider.gameObject;
+                    enemySelected = true;
+
+                }
+
+                if (playerSelected == true)
+                {
+                    agent.SetDestination(hit.point);
+                    enemySelected = false;
+                }
+
+            }
+        }
+
+        
+
+        
+
+
+
+    }
+
+    private void findEnemy()
+    {
+        if (enemySelected)
+        {
+            if (selectedEnemy != null)
+            {
+                agent.SetDestination(selectedEnemy.transform.position);
+            }
+            else
+            {
+                agent.SetDestination(this.transform.position);
+                enemySelected = false;
+            }
+
+        }
+    }
+}
